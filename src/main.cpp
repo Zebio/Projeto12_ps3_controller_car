023a1f5c028765e421a8112ef5bac5a3248c11dc
Zebio <mateuseusebio@gmail.com>
2021-09-21 20:42:42 -0300

    otimização do codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <Arduino.h>
 #include <Ps3Controller.h>
+#include <stdlib.h>
 
 
 #define sig_pwm_tracao 2
@@ -18,16 +19,31 @@ int battery = 0;
 
 void notify()
 {
-    //--- Digital cross/square/triangle/circle button events ---
-    if( Ps3.event.button_down.cross )
-        Serial.println("Started pressing the cross button");
-    if( Ps3.event.button_up.cross )
-        Serial.println("Released the cross button");
+        if (Ps3.data.analog.stick.lx>10)
+       { 
+           digitalWrite(sig_dir_dir,1);
+           digitalWrite(sig_dir_esq,0);
 
-    if( Ps3.event.button_down.square )
-        Serial.println("Started pressing the square button");
-    if( Ps3.event.button_up.square )
-        Serial.println("Released the square button");
+       }
+       else if (Ps3.data.analog.stick.lx<-10)
+       {
+           digitalWrite(sig_dir_dir,0);
+           digitalWrite(sig_dir_esq,1);
+       }
+
+    int8_t aux= Ps3.data.analog.stick.lx;
+    if(aux<0)
+        aux=aux*-1;
+
+    uint8_t aux2=(uint8_t)aux;
+
+    if(aux2>10)
+        ledcWrite(1,aux2*2);
+    else    
+        ledcWrite(1, 0);
+
+    //--- Digital cross/square/triangle/circle button events ---
+
 
     if( Ps3.event.button_down.triangle )
     {
@@ -36,43 +52,6 @@ void notify()
     }
     if( Ps3.event.button_up.triangle )
         Serial.println("Released the triangle button");
-
-    if( Ps3.event.button_down.circle )
-        Serial.println("Started pressing the circle button");
-    if( Ps3.event.button_up.circle )
-        Serial.println("Released the circle button");
-
-    //--------------- Digital D-pad button events --------------
-    if( Ps3.event.button_down.up )
-        Serial.println("Started pressing the up button");
-    if( Ps3.event.button_up.up )
-        Serial.println("Released the up button");
-
-    if( Ps3.event.button_down.right )
-        Serial.println("Started pressing the right button");
-    if( Ps3.event.button_up.right )
-        Serial.println("Released the right button");
-
-    if( Ps3.event.button_down.down )
-        Serial.println("Started pressing the down button");
-    if( Ps3.event.button_up.down )
-        Serial.println("Released the down button");
-
-    if( Ps3.event.button_down.left )
-        Serial.println("Started pressing the left button");
-    if( Ps3.event.button_up.left )
-        Serial.println("Released the left button");
-
-    //------------- Digital shoulder button events -------------
-    if( Ps3.event.button_down.l1 )
-        Serial.println("Started pressing the left shoulder button");
-    if( Ps3.event.button_up.l1 )
-        Serial.println("Released the left shoulder button");
-
-    if( Ps3.event.button_down.r1 )
-        Serial.println("Started pressing the right shoulder button");
-    if( Ps3.event.button_up.r1 )
-        Serial.println("Released the right shoulder button");
 
     //-------------- Digital trigger button events -------------
     if( Ps3.event.button_down.l2 )
@@ -100,50 +79,13 @@ void notify()
         digitalWrite(sig_frente,0);
     }
 
-    //--------------- Digital stick button events --------------
-    if( Ps3.event.button_down.l3 )
-        Serial.println("Started pressing the left stick button");
-    if( Ps3.event.button_up.l3 )
-        Serial.println("Released the left stick button");
-
-    if( Ps3.event.button_down.r3 )
-        Serial.println("Started pressing the right stick button");
-    if( Ps3.event.button_up.r3 )
-        Serial.println("Released the right stick button");
-
-    //---------- Digital select/start/ps button events ---------
-    if( Ps3.event.button_down.select )
-        Serial.println("Started pressing the select button");
-    if( Ps3.event.button_up.select )
-        Serial.println("Released the select button");
-
-    if( Ps3.event.button_down.start )
-        Serial.println("Started pressing the start button");
-    if( Ps3.event.button_up.start )
-        Serial.println("Released the start button");
-
-    if( Ps3.event.button_down.ps )
-        Serial.println("Started pressing the Playstation button");
-    if( Ps3.event.button_up.ps )
-        Serial.println("Released the Playstation button");
 
 
     //---------------- Analog stick value events ---------------
    if( abs(Ps3.event.analog_changed.stick.lx) + abs(Ps3.event.analog_changed.stick.ly) > 2 ){
        Serial.print("Moved the left stick:");
        Serial.print(" x="); Serial.print(Ps3.data.analog.stick.lx, DEC);
-       if (Ps3.data.analog.stick.lx>0)
-       { 
-           digitalWrite(sig_dir_dir,1);
-           digitalWrite(sig_dir_esq,0);
 
-       }
-       else
-       {
-           digitalWrite(sig_dir_dir,0);
-           digitalWrite(sig_dir_esq,1);
-       }
-       ledcWrite(1, Ps3.data.analog.stick.lx+(int8_t)128);
        Serial.print(" y="); Serial.print(Ps3.data.analog.stick.ly, DEC);
        Serial.println();
     }
@@ -157,70 +99,8 @@ void notify()
        Serial.println();
    }
 
-   //--------------- Analog D-pad button events ----------------
-   if( abs(Ps3.event.analog_changed.button.up) ){
-       Serial.print("Pressing the up button: ");
-       Serial.println(Ps3.data.analog.button.up, DEC);
-   }
 
-   if( abs(Ps3.event.analog_changed.button.right) ){
-       Serial.print("Pressing the right button: ");
-       Serial.println(Ps3.data.analog.button.right, DEC);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.down) ){
-       Serial.print("Pressing the down button: ");
-       Serial.println(Ps3.data.analog.button.down, DEC);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.left) ){
-       Serial.print("Pressing the left button: ");
-       Serial.println(Ps3.data.analog.button.left, DEC);
-   }
-
-   //---------- Analog shoulder/trigger button events ----------
-   if( abs(Ps3.event.analog_changed.button.l1)){
-       Serial.print("Pressing the left shoulder button: ");
-       Serial.println(Ps3.data.analog.button.l1, DEC);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.r1) ){
-       Serial.print("Pressing the right shoulder button: ");
-       Serial.println(Ps3.data.analog.button.r1, DEC);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.l2) ){
-       Serial.print("Pressing the left trigger button: ");
-       Serial.println(Ps3.data.analog.button.l2, DEC);
-       ledcWrite(0, Ps3.data.analog.button.l2);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.r2) ){
-       Serial.print("Pressing the right trigger button: ");
-       Serial.println(Ps3.data.analog.button.r2, DEC);
-       ledcWrite(0, Ps3.data.analog.button.r2);
-   }
-
-   //---- Analog cross/square/triangle/circle button events ----
-   if( abs(Ps3.event.analog_changed.button.triangle)){
-       Serial.print("Pressing the triangle button: ");
-       Serial.println(Ps3.data.analog.button.triangle, DEC);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.circle) ){
-       Serial.print("Pressing the circle button: ");
-       Serial.println(Ps3.data.analog.button.circle, DEC);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.cross) ){
-       Serial.print("Pressing the cross button: ");
-       Serial.println(Ps3.data.analog.button.cross, DEC);
-   }
-
-   if( abs(Ps3.event.analog_changed.button.square) ){
-       Serial.print("Pressing the square button: ");
-       Serial.println(Ps3.data.analog.button.square, DEC);
-   }
+   
 
    //---------------------- Battery events ---------------------
     if( battery != Ps3.data.status.battery ){
